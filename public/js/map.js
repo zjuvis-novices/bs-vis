276204295e9579cfc3341eed160bcc89a09a6310
Celestial Phineas <hanghang0713@hotmail.com>
2018-04-26 23:07:41 +0800

    Switch to AMap

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,7 @@
 // Initialize AMap Loca
-var loca = new Loca('container', {
+var map = new AMap.Map('container', {
     resizeEnable: true,
     zoom: 10,
     center:[120.2,30.25],
     mapStyle: 'amap://styles/grey'
-});
-
-// Get the visualization layer
-var visualLayer = new Loca.VisualLayer({
-    container: loca,
-    type: 'point',
-    shape: 'circle'
 });
