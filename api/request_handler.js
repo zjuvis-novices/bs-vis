fc7fb3c3c9599c19dac9e69019d292193b8d1c84
Celestial Phineas <hanghang0713@hotmail.com>
2018-04-27 12:07:41 +0800

    RESTful

--- api/request_handler.js
+++ api/request_handler.js
@@ -11,14 +11,13 @@ function dateStr2Index(dateStr, hour) {
     return (dateObj.valueOf() - epoch.valueOf())/3600000;
 }
 
-function handleTraffic(req, res) {
+function getTraffic(req, res) {
     // Get parameter list
-    var params          = url.parse(req.url, true).query;
     var responseExpr    = null;
-    var type            = params['type'];
-    var weekday         = params['weekday'];
+    var type            = req.params['type'];
+    var weekday         = req.params['weekday'];
     if(weekday) weekday = weekday.toUpperCase();
-    var hour            = parseInt(params['hour']);
+    var hour            = parseInt(req.params['hour'].split('.')[0]);
     if(type === 'speed') {
         responseExpr = dataModel.trafficData.getSpeed(weekday, hour);
     } else if(type === 'density') {
@@ -32,7 +31,7 @@ function handleTraffic(req, res) {
     res.end(JSON.stringify(responseExpr));
 }
 
-function handlePOI(req, res) {
+function getPOI(req, res) {
     res.statusCode = 200;
     res.set({
         'Content-Type': 'application/json;charset=UTF-8',
@@ -41,5 +40,27 @@ function handlePOI(req, res) {
     res.end(JSON.stringify(dataModel.poiData));
 }
 
-exports.handleTraffic   = handleTraffic;
-exports.handlePOI       = handlePOI;
+function getWeather(req, res) {
+    var type = req.params['type'];
+    if(type && typeof type !== 'string') {
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    type = type.toLowerCase().split('.')[0];
+    if(dataModel.weatherData[type] === undefined) {
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    res.statusCode = 200;
+    res.set({
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Cache-Control': 'public, max-age=31557600'
+    });
+    res.end(JSON.stringify(dataModel.weatherData[type]));
+}
+
+exports.getTraffic      = getTraffic;
+exports.getPOI          = getPOI;
+exports.getWeather      = getWeather;
--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const port = 8080;
 // Static pages
 app.use(express.static('public'));
 
-// API
+// API routing
 // Traffic
-app.get('/api/traffic.json', (req, res) => {
-    requestHandler.handleTraffic(req, res);
-});
+app.get('/api/traffic/:type/:weekday/:hour', requestHandler.getTraffic);
 
 // POI
-app.get('/api/poi.json', (req, res) => {
-    requestHandler.handlePOI(req, res);
-});
+app.get('/api/poi', requestHandler.getPOI);
+app.get('/api/poi.*', requestHandler.getPOI);
+
+// Weather
+app.get('/api/weather/:type',  requestHandler.getWeather);
 
 var server = app.listen(port, () => {
     console.log('Server running at ' + hostname + ':' + port);
