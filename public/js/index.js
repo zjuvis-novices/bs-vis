d5f7be9c9e7c4fbdaa432277a15890ea8757b37f
wty <973920187@qq.com>
2018-05-05 21:18:08 +0800

    not using loc the bubblemap easy to get stuck

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,18 @@ var currenttemp = null;
 var currentPPD = null;
 var temperaturedata = null;
 var PPDdata = null;
-var speeddatacache = [];
 var speeddata = [];
 var positivedata = [];
 var negativedata = [];
 var tirednessdata = [];
 var densitydata = null;
-var myChart = null;
+var positive_raw = [];
+var negative_raw = [];
+var tiredness_raw = [];
+var pos = null;
+var neg = null;
+var tir = null;
+var show_emotion = {pos: false, neg: false, tir: false}
 $.get('api/weather/temperature.json', function(data){
     temperaturedata = data;
     });
@@ -60,33 +65,67 @@ function refreshHeatmap(){
         max: 100
     });
 }
-
+//create emotion object
+function create_emotion(loc_list, color){
+    var result = [];
+    for(var i = 0; i < loc_list.length; i++)
+    {
+        result[i] = new AMap.Circle(
+            {center:new AMap.LngLat(loc_list[i][0],loc_list[i][1]),
+                radius:loc_list[i][2],fillOpacity:0.2,strokeOpacity:0.3,
+                strokeWeight:1,fillColor:color,strokeColor:color});
+    }
+    return result;
+}
+//deal with toggle action of emotion switches
+function toggle_emotion(emotion) {
+    if(show_emotion[emotion]){
+        show_emotion[emotion] = false;
+        for(var i=0; i<1856; i++)
+        {
+            eval(emotion+"[i].hide()")
+        }
+    }
+    else{
+        show_emotion[emotion] = true;
+        for(var i=0; i<1856; i++)
+        {
+            eval("console.log("+emotion+")")
+            console.log("!!!!")
+            eval(emotion+"[i].show()")
+        }
+    }
+}
 //refreshBubblemap
 function refreshBubblemap(){
-    var bubblemapdata = []
-    //convert data
-    for(var i=0; i<1856; i++){
-        bubblemapdata.push({
-            name: poiData[i]['name'],
-            value: poiData[i]['location'].concat(speeddata[i+parseInt($('#hour').val())*24])
-        })
+    refreshEmotionSet()
+    var positive = positive_raw.slice(1856*parseInt($("#hour").val()), 1856*parseInt($("#hour").val())+1856)
+   console.log(positive)
+    var negative = negative_raw.slice(1856*parseInt($("#hour").val()), 1856*parseInt($("#hour").val())+1856)
+    var tiredness = tiredness_raw.slice(1856*parseInt($("#hour").val()), 1856*parseInt($("#hour").val())+1856)
+    pos = create_emotion(positive, "#db4d6d")
+    neg = create_emotion(negative, "#3316b3")
+    tir = create_emotion(tiredness, "#f7d977")
+    console.log(pos)
+    for(var i = 0; i <1856; i++)
+    {
+        pos[i].setMap(map);
+        pos[i].show();
+        neg[i].setMap(map);
+        neg[i].show();
+        tir[i].setMap(map);
+        tir[i].hide();
     }
-    console.log(bubblemapdata)
 
-    setTimeout(()=>{
-    myChart.setOption({
-        series: [
-            {
-                name: 'speed',
-                type: 'scatter',
-                coordinateSystem: 'bmap',
-                data: bubblemapdata
-            }]
-    })},500)
+   // toggle_emotion('pos');
+    /*
+    toggle_emotion('neg');
+    toggle_emotion('tir');*/
 }
 
 // Time changing event
 function onTimeChange() {
+
     $('#time-string').text($('#hour').val() + ':00');
     //console.log(positivedata)
     var currentdate = globalDate.getDate();
@@ -121,10 +160,32 @@ function onTimeChange() {
 
     if($("#displaytype").val() == 2)
         refreshHeatmap();
-    else{
-        //refreshBubblemap();
+    else if($("#displaytype").val() == 1){
+        refreshBubblemap();
     }
 
+}
+
+function refreshEmotionSet(){
+    positive_raw = [];
+    negative_raw = [];
+    tiredness_raw = [];
+    var tmp1 = [];
+    var tmp2 = [];
+    var tmp3 = [];
+    console.log(positivedata)
+    for(var i=0; i<1856*24; i++){
+        tmp1 = [];
+        tmp2= [];
+        tmp3 = [];
+        tmp1 = poiData[i%1856]["location"].concat(1000*positivedata[i])
+        tmp2 = poiData[i%1856]["location"].concat(1000*negativedata[i])
+        tmp3 = poiData[i%1856]["location"].concat(1000*tirednessdata[i])
+        positive_raw.push(tmp1)
+        negative_raw.push(tmp2)
+        tiredness_raw.push(tmp3)
+    }
+   // console.log(positive_raw)
 }
 
 function initDatePicker(elems) {
@@ -165,23 +226,23 @@ function initDatePicker(elems) {
                 case 5: weekday = "friday"; break;
                 case 6: weekday = "saturday"; break;
             }
-                for(var i = 0; i<24; i++)
-                {
-                    //setTimeout(get(i,speeddata, weekday), 500);
-                    $.get('api/traffic/speed/'+weekday+'/'+i, function (data) {
-                        speeddata = speeddata.concat(data);
-                    })
-                    $.get('api/emotion/positive/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
-                        positivedata = positivedata.concat(data);
-                    })
-                    $.get('api/emotion/negative/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
-                        negativedata = negativedata.concat(data);
-                    })
-                    $.get('api/emotion/tiredness/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
-                        tirednessdata = tirednessdata.concat(data);
-                    })
-
-                }
+            for(var i = 0; i<24; i++)
+            {
+                //setTimeout(get(i,speeddata, weekday), 500);
+                $.get('api/traffic/speed/'+weekday+'/'+i, function (data) {
+                    speeddata = speeddata.concat(data);
+                })
+                $.get('api/emotion/positive/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
+                    positivedata = positivedata.concat(data);
+                })
+                $.get('api/emotion/negative/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
+                    negativedata = negativedata.concat(data);
+                })
+                $.get('api/emotion/tiredness/'+globalDate.getFullYear()+'-'+(parseInt(globalDate.getMonth())+1)+'-'+globalDate.getDate()+'/'+i, function (data) {
+                    tirednessdata = tirednessdata.concat(data);
+                })
+            }
+           // refreshEmotionSet();
 
 
 
