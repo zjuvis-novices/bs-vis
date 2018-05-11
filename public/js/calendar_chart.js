7787241834df686ec5f495dfc4fc050d50dbd607
wty <973920187@qq.com>
2018-05-11 10:35:15 +0800

    add calendar

--- public/js/calendar_chart.js
+++ public/js/calendar_chart.js
@@ -1,1 +1,18 @@
-
+var calendar;
+var positiveByHour = [];
+var negativeByHour = [];
+var tirednessByHour = [];
+// The initialization of data options should be delayed until the average
+// data get requests are done.
+$.when(
+    positiveAverageGet,
+    negativeAverageGet,
+    tirednessAverageGet
+).done(function () {
+    // These data are only available when the initialization is done
+    positiveByHour = positiveAverageGet.responseJSON;
+    negativeByHour = negativeAverageGet.responseJSON;
+    tirednessByHour = tirednessAverageGet.responseJSON;
+    // Do initial data/option binding
+    //calendar.setOption(calendarOptions);
+});
