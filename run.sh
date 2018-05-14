1925898417caed653062e27f55bb628f0bfe76d3
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-14 17:01:37 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-cp ./nginx.conf /etc/nginx/sites-enabled/default
+mkdir /etc/nginx/sites-enabled
+cp ./nginx.conf /etc/nginx/sites-enabled/default.conf
 forever start app.js
 nohub node app.js > ~/log/node.log 2>&1 &
 nginx
