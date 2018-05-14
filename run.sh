df8429832646e73d7df9512c76d90b6a099a7fd9
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-14 17:19:45 +0800

    config

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-nginx -t
-kill -HUP `cat /usr/local/nginx/nginx.pid`
+kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
 cp ./nginx.conf /usr/local/nginx/conf/nginx.conf
+nginx -t
 forever start app.js
 nohub node app.js > ~/log/node.log 2>&1 &
 nginx
