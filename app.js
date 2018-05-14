65cf2559bd53a1a5d2cfdc3638767fa7df4a2b16
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-14 18:05:03 +0800

    fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 const hostname = 'localhost';
 const port = 8080;
 
-var path = 'city-emotion';
+var path = '/city-emotion';
 
 // Static pages
 app.use(path, express.static('public'));
