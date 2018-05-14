abe1565c2b6eeb83e096ebfe960953e6f60ced8c
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-14 18:10:41 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
 cp ./nginx.conf /usr/local/nginx/conf/nginx.conf
 nginx -t
+forever stop app.js
 forever start app.js
-nohub node app.js > ~/log/node.log 2>&1 &
 nginx
